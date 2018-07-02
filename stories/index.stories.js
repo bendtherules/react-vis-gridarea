e405b0ff90a754e0f860d7251af4de2af2d9b6d6
Abhas Bhattacharya <abhasbhattacharya2@gmail.com>
2018-07-02 11:08:05 +0530

    Remove one demo story + debugger statement

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -10,16 +10,6 @@ import ChartWithGridAreaVertical from './BasicGridAreaVertical';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ));
-
 storiesOf('Grid area', module)
   .add('Basic Horizontal', () => <ChartWithGridAreaHorizontal onClick={action('clicked')} />)
   .add('Basic Vertical', () => <ChartWithGridAreaVertical onClick={action('clicked')} />)
