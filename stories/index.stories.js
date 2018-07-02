6c444beadfd9f86192189ddf1ff5f027c99fd072
Abhas Bhattacharya <abhasbhattacharya2@gmail.com>
2018-07-02 11:14:51 +0530

    Add default style story

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -7,10 +7,11 @@ import { linkTo } from '@storybook/addon-links';
 import { Button, Welcome } from '@storybook/react/demo';
 import ChartWithGridAreaHorizontal from './BasicGridAreaHorizontal';
 import ChartWithGridAreaVertical from './BasicGridAreaVertical';
+import ChartWithGridAreaDefaultColor from './GridAreaWithDefaultStyle';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 storiesOf('Grid area', module)
   .add('Basic Horizontal', () => <ChartWithGridAreaHorizontal onClick={action('clicked')} />)
   .add('Basic Vertical', () => <ChartWithGridAreaVertical onClick={action('clicked')} />)
-
+  .add('With default style', () => <ChartWithGridAreaDefaultColor onClick={action('clicked')} />)
